eed3db0db034a82343e6556044af1fbfefdb73d9
sanskar sontakke <sanskarsontakke06@gmail.com>
2025-10-05 11:51:06 +0530

    manage zindex in developer section

--- Frontend/src/components/Developer.jsx
+++ Frontend/src/components/Developer.jsx
@@ -11,28 +11,27 @@ const DeveloperPage = ({ setCurrentPage }) => {
       <Background />
 
       {/* Navbar at top */}
-      <div className="absolute top-0 w-full z-10">
+      <div className="absolute top-0 w-full z-20">
         <Navbar setCurrentPage={setCurrentPage} />
       </div>
 
       {/* Centered ProfileCard with top margin */}
       <div className="flex items-center justify-center flex-1 w-full z-10 px-2 mt-15">
-       <ProfileCard
-  name="Sontakke Sanskar"
-  title="Software Engineer"
-  handle="Gmail"
-  status="Online"
-  contactText="Contact Me"
-  avatarUrl={me}
-  showUserInfo={true}
-  enableTilt={true}
-  enableMobileTilt={false}
-  onContactClick={() => console.log("Contact clicked")}
-  innerGradient="linear-gradient(135deg, hsl(184, 70%, 21%) 0%, hsl(184, 70%, 31%) 100%)"
-  behindGradient="linear-gradient(135deg, hsl(184, 70%, 21%) 0%, hsl(184, 70%, 31%) 100%)"
-  className="max-w-md md:max-w-lg w-full scale-105 hover:scale-110 transition-transform duration-500 h-[350px]" // adjust height as needed
-/>
-
+        <ProfileCard
+          name="Sontakke Sanskar"
+          title="Software Engineer"
+          handle="Gmail"
+          status="Online"
+          contactText="Contact Me"
+          avatarUrl={me}
+          showUserInfo={true}
+          enableTilt={true}
+          enableMobileTilt={false}
+          onContactClick={() => console.log("Contact clicked")}
+          innerGradient="linear-gradient(135deg, hsl(184, 70%, 21%) 0%, hsl(184, 70%, 31%) 100%)"
+          behindGradient="linear-gradient(135deg, hsl(184, 70%, 21%) 0%, hsl(184, 70%, 31%) 100%)"
+          className="max-w-md md:max-w-lg w-full scale-105 hover:scale-110 transition-transform duration-500 h-[350px]" // adjust height as needed
+        />
       </div>
     </div>
   );
